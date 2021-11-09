2650446a78dc90a9c25a391ffa19ed0a1ba49621
Андрей <lapihin.andrei@yandex.ru>
2021-11-09 12:22:18 +0300

    1.2. Вызов метода объекта [#166085 #226513]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -1,11 +1,11 @@
 package ru.job4j.oop;
 
-import java.sql.SQLOutput;
 
 public class Student {
     public void music() {
         System.out.println("Tra tra tra");
     }
+
     public void song() {
         System.out.println("I belive i can fly");
     }
