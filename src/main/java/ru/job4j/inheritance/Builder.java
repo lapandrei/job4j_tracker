40dc87b222692f25c47f8fa68f97ce8b518c5024
Андрей <lapihin.andrei@yandex.ru>
2021-11-17 15:15:57 +0300

    3. Реализация профессий в коде [#6837]

---
+++ src/main/java/ru/job4j/inheritance/Builder.java
@@ -0,0 +1,24 @@
+package ru.job4j.inheritance;
+
+public class Builder extends Engineer {
+    private int finishedProject;
+
+    public Builder() {
+
+    }
+
+    public Builder(String name, String surname, String education, String birthday, String work,
+                      int finishedProject) {
+        super(name, surname, education, birthday, work);
+        this.finishedProject = finishedProject;
+    }
+
+    public int getFortranDeveloper() {
+        return this.finishedProject;
+    }
+
+    public boolean buildHouse(String title) {
+        boolean rsl = false;
+        return rsl;
+    }
+}
