6462710c4e771f58a7fb8d41009d3b61d47b0f01
Андрей <lapihin.andrei@yandex.ru>
2021-11-21 17:15:44 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,32 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                itemsWithoutNull[size] = items[i];
+                size++;
+            }
+        }
+        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, size);
+        return itemsWithoutNull;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemsByName = new Item[items.length];
+        int size = 0;
+        for (int i = 0; i < this.size; i++) {
+            if (items[i].getName().equals(key)) {
+                itemsByName[size] = items[i];
+                size++;
+            }
+        }
+        itemsByName = Arrays.copyOf(itemsByName, size);
+        return itemsByName;
     }
 
     public Item findById(int id) {
@@ -21,5 +49,23 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public static void main(String[] args) {
+        Tracker tracker = new Tracker();
+        Item first = new Item("First");
+        Item second = new Item("Second");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(new Item("First"));
+        tracker.add(new Item("Second"));
+        tracker.add(new Item("First"));
+        Item[] result = tracker.findAll();
+        /*for (int i = 0; i < 5; i++) {
+            System.out.println(result[i].getName());
+
+        }*/
+        String key = first.getName();
+        System.out.println(key);
     }
 }
