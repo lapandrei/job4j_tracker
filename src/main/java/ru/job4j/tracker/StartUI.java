dad0ff838d3ea1fed1f3ceaad455cec0a244d6d2
Андрей <lapihin.andrei@yandex.ru>
2021-12-03 15:40:54 +0300

    4.1. Разрыв зависимости StartUI от Scanner. [#181778 #234458]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,7 +3,8 @@ package ru.job4j.tracker;
 public class StartUI {
     public static void createItem(Input input, Tracker tracker) {
         System.out.println("=== Create a new Item ===");
-        Item item = new Item(input.askStr("Enter name: "));
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
         tracker.add(item);
         System.out.println("Добавленная заявка: " + item);
     }
@@ -23,7 +24,8 @@ public class StartUI {
     public static void editItem(Input input, Tracker tracker) {
         System.out.println("=== Edit item ===");
         int id = input.askInt("Enter id: ");
-        Item item = new Item(input.askStr("Enter name: "));
+        String name = input.askStr("Enter name: ");
+        Item item = new Item(name);
         if (tracker.replace(id, item)) {
             System.out.println("Заявка изменена успешно.");
         } else {
@@ -33,7 +35,8 @@ public class StartUI {
 
     public static void deleteItem(Input input, Tracker tracker) {
         System.out.println("=== Delete item ===");
-        if (tracker.delete(input.askInt("Enter id: "))) {
+        int id = input.askInt("Enter id: ");
+        if (tracker.delete(id)) {
             System.out.println("Заявка удалена успешно.");
         } else {
             System.out.println("Ошибка удаления заявки.");
