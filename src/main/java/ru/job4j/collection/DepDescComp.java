ec7e8e39140c78ddd44c5bff798fb2ae51b47e0e
lapandrei <lapihin.andrei@yandex.ru>
2022-07-23 20:32:12 +0300

    2. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String[] first = o1.split("/");
+        String[] two = o2.split("/");
+        int result = two[0].compareTo(first[0]);
+        if (result == 0) {
+            result = o1.compareTo(o2);
+        }
+        return result;
+    }
+}
