e851aa5bab678396ff670d3ab401f4eaa0fe62df
lapandrei <lapihin.andrei@yandex.ru>
2024-06-18 13:55:20 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,21 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    public static final String NEW_VALUE = "";
+    private String surname;
+    private int sizeOfEmpty = 10;
+    private String name;
+
+    Broken() { }
+
+    void echo() { }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) { }
+}
